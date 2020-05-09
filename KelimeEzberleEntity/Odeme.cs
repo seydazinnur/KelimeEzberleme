8a74026b2b782ebc0916d841475e60824e7f7290
seydazinnur <34726875+seydazinnur@users.noreply.github.com>
2020-05-10 00:48:37 +0300

    Add files via upload

---
+++ KelimeEzberleEntity/Odeme.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KelimeEzberleEntity
+{
+    public class Odeme : BaseClass
+    {
+        public int OdemeID { get; set; }
+        public string OdemeTuru { get; set; }
+ 
+        //sıkıntı
+        public List<Kullanici> Kullanicilar { get; set; }
+        public KullaniciOdeme KullaniciOdeme { get; set; }
+    }
+}
